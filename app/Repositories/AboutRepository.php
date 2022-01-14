ff9721c26d65e1e2fa0c504b1d0314e4ae2ead03
fmshot2 <femishotolaa@gmail.com>
2022-01-14 10:48:33 +0100

    changed about to first

--- app/Repositories/AboutRepository.php
+++ app/Repositories/AboutRepository.php
@@ -5,34 +5,34 @@ namespace App\Repositories;
 use App\Interfaces\AboutRepositoryInterface;
 use App\Models\About;
 
-class AboutRepository implements AboutRepositoryInterface 
+class AboutRepository implements AboutRepositoryInterface
 {
-    public function getAllAbouts() 
+    public function getAllAbouts()
     {
-        return About::all();
+        return About::first();
     }
 
-    public function getAboutById($aboutId) 
+    public function getAboutById($aboutId)
     {
         return About::findOrFail($aboutId);
     }
 
-    public function deleteAbout($aboutId) 
+    public function deleteAbout($aboutId)
     {
         About::destroy($aboutId);
     }
 
-    public function createAbout(array $aboutDetails) 
+    public function createAbout(array $aboutDetails)
     {
         return About::create($aboutDetails);
     }
 
-    public function updateAbout($aboutId, array $newDetails) 
-    {        
+    public function updateAbout($aboutId, array $newDetails)
+    {
         return About::whereId($aboutId)->update($newDetails);
     }
 
-    // public function getFulfilledOrders() 
+    // public function getFulfilledOrders()
     // {
     //     return Order::where('is_fulfilled', true);
     // }
