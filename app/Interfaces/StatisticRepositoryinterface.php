b2f04ebbe28457dd9a5c943e9e9082c2bba3477d
fmshot2 <femishotolaa@gmail.com>
2022-01-17 19:06:16 +0100

    fixed statitic error

--- app/Interfaces/StatisticRepositoryinterface.php
+++ app/Interfaces/StatisticRepositoryinterface.php
@@ -5,7 +5,7 @@ namespace App\Interfaces;
 interface StatisticRepositoryInterface
 {
     public function getAllStatistics();
-    // public function getStatisticById($statisticId);
+    public function getStatisticById($statisticId);
     // public function deleteStatistic($statisticId);
     // public function createStatistic(array $statisticDetails);
     // public function updateStatistic($statisticId, array $newDetails);
--- app/Repositories/StatisticRepository.php
+++ app/Repositories/StatisticRepository.php
@@ -2,13 +2,22 @@
 
 namespace App\Repositories;
 
+
 use App\Interfaces\StatisticRepositoryInterface;
+
 use App\Models\Statistic;
 
 class StatisticRepository implements StatisticRepositoryInterface
 {
+
     public function getAllStatistics()
     {
         return Statistic::first();
     }
+
+    public function getStatisticById($statisticsId)
+    {
+        return Service::findOrFail($statisticsId);
+    }
+
 }
