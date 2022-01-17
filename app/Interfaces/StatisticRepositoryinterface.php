abf332d8617e77723830b23f4d47cf2a1660221c
fmshot2 <femishotolaa@gmail.com>
2022-01-17 18:05:03 +0100

    renamed statitic interface

--- app/Interfaces/StatisticRepositoryinterface.php
+++ app/Interfaces/StatisticRepositoryinterface.php
@@ -4,6 +4,10 @@ namespace App\Interfaces;
 
 interface StatisticRepositoryInterface
 {
-
     public function getAllStatistics();
+    public function getStatisticById($statisticId);
+    public function deleteStatistic($statisticId);
+    public function createStatistic(array $statisticDetails);
+    public function updateStatistic($statisticId, array $newDetails);
+    // public function getFulfilledOrders();
 }
--- app/Repositories/StatisticRepository.php
+++ app/Repositories/StatisticRepository.php
@@ -3,13 +3,12 @@
 namespace App\Repositories;
 
 use App\Interfaces\StatisticRepositoryInterface;
-
 use App\Models\Statistic;
 
 class StatisticRepository implements StatisticRepositoryInterface
 {
     public function getAllStatistics()
-        {
-            return Statistic::all();
-        }
+    {
+        return Statistic::all();
+    }
 }
