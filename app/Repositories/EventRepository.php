92286c6418e47b93cbf999f85b7c69e8bd0ec9ec
fmshot2 <femishotolaa@gmail.com>
2022-01-27 16:56:19 +0100

    removed carbon foreach in getPreviousEvent

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -42,14 +42,14 @@ class EventRepository implements EventRepositoryInterface
     public function getPreviousEvent()
     {
         $previousEvents = Event::whereDate('date', '<', Carbon::now())->get();
-        foreach ($previousEvents as $previousEvent => $value) {
-            $date = Carbon::parse($previousEvent['date']);
-            $previousEvent['date'] = Carbon::createFromFormat('d/m/Y', $date);
+//         foreach ($previousEvents as $previousEvent => $value) {
+//             $date = Carbon::parse($previousEvent['date']);
+//             $previousEvent['date'] = Carbon::createFromFormat('d/m/Y', $date);
 
-//             $dateString = '25/08/2017';
-// $dateObject = \Carbon::createFromFormat('d/m/Y', $dateString);
+// //             $dateString = '25/08/2017';
+// // $dateObject = \Carbon::createFromFormat('d/m/Y', $dateString);
 
-        }
+//         }
         return $previousEvents;
     }
 
